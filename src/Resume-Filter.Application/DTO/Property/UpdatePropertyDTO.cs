59f8f8869419e0e92503748da05fddcdd64d398c
Jurayevkh <jurayevkh@gmail.com>
2024-07-23 03:27:00 +0500

    Id Prop added to Property DTO

--- src/Resume-Filter.Application/DTO/Property/UpdatePropertyDTO.cs
+++ src/Resume-Filter.Application/DTO/Property/UpdatePropertyDTO.cs
@@ -2,6 +2,7 @@
 
 public class UpdatePropertyDTO
 {
+    public int Id { get; set; }
     public int? VacancyId { get; set; }
     public string? Name { get; set; }
     public int? Value { get; set; }
