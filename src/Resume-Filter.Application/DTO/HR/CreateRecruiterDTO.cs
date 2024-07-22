012ae77ca3ad9968c34b7494bd0ef2be419092d2
Jurayevkh <jurayevkh@gmail.com>
2024-07-23 03:26:31 +0500

    Added Password prop to Recruiter

--- src/Resume-Filter.Application/DTO/HR/CreateRecruiterDTO.cs
+++ src/Resume-Filter.Application/DTO/HR/CreateRecruiterDTO.cs
@@ -5,5 +5,6 @@ public class CreateRecruiterDTO
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
 }
 
