1825fc72fff5e7487fc0ee7fd22b69a72a927caa
Jurayevkh <jurayevkh@gmail.com>
2024-07-23 03:30:37 +0500

    Dependency injection of application layer to depen api layer

---
+++ src/Resume-Filter.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Resume_Filter.Application.Mappers;
+using System.Reflection;
+
+namespace Resume_Filter.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddAutoMapper(typeof(MappingConfiguration));
+        return services;
+    }
+}
+
