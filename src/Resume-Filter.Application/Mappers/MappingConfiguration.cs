dcfd38a9e9cc40712065828ae3890d744d8505eb
Jurayevkh <jurayevkh@gmail.com>
2024-07-22 21:00:18 +0500

    Mapping Configurations

--- src/Resume-Filter.Application/Mappers/MappingConfiguration.cs
+++ src/Resume-Filter.Application/Mappers/MappingConfiguration.cs
@@ -1,7 +1,34 @@
-﻿namespace Resume_Filter.Application.Mappers;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Resume_Filter.Application.DTO.HR;
+using Resume_Filter.Application.DTO.Property;
+using Resume_Filter.Application.DTO.Vacancy;
+using Resume_Filter.Application.UseCases.Commands.HR;
+using Resume_Filter.Application.UseCases.Commands.Property;
+using Resume_Filter.Application.UseCases.Commands.Vacancy;
+using Resume_Filter.Domain.Entities.HR;
+using Resume_Filter.Domain.Entities.Vacancy;
 
-public class MappingConfiguration
+namespace Resume_Filter.Application.Mappers;
+
+public class MappingConfiguration:Profile
 {
+    public MappingConfiguration()
+    {
+        CreateMap<CreateRecruiter, CreateRecruiterDTO>().ReverseMap();
+        CreateMap<UpdateRecruiter, UpdateRecruiterDTO>().ReverseMap();
 
+        CreateMap<CreateProperty, CreatePropertyDTO>().ReverseMap();
+        CreateMap<UpdateProperty, UpdatePropertyDTO>().ReverseMap();
+
+        CreateMap<CreateVacancy, CreateVacancyDTO>().ReverseMap();
+        CreateMap<UpdateVacancy, UpdateVacancyDTO>().ReverseMap();
+
+        //---------------
+
+        CreateMap<CreateVacancy, Vacancies>().ReverseMap();
+        CreateMap<CreateRecruiter, Recruiter>().ReverseMap();
+        CreateMap<CreateProperty, Property>().ReverseMap();
+    }
 }
 
