735f6ca88d39f93c25dde02dc659387c3fd7b070
Jurayevkh <jurayevkh@gmail.com>
2024-07-22 13:27:25 +0500

    Property entity created

---
+++ src/Resume-Filter.Domain/Entities/Property/Properties.cs
@@ -0,0 +1,9 @@
+﻿namespace Resume_Filter.Domain.Entities.Property;
+
+public class Properties:BaseEntity
+{
+    public int VacancyId { get; set; }
+    public string Name { get; set; }
+    public int Value { get; set; }
+}
+
