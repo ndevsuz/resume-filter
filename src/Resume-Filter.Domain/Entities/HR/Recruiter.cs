e8cf19593c84381073f096f8d70508d3a49f970e
Jurayevkh <jurayevkh@gmail.com>
2024-07-22 13:27:10 +0500

    Recruiter entity created

---
+++ src/Resume-Filter.Domain/Entities/HR/Recruiter.cs
@@ -0,0 +1,9 @@
+﻿namespace Resume_Filter.Domain.Entities.HR;
+
+public class Recruiter : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
+
