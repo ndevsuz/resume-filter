a11834b0ae54e609e827955c59fcd9fccd4e4272
Jurayevkh <jurayevkh@gmail.com>
2024-07-22 13:31:14 +0500

    Dependency Injection of Infrastructure layer

---
+++ src/Resume-Filter.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Resume_Filter.Application.Abstractions;
+using Resume_Filter.Infrastructure.Data;
+
+namespace Resume_Filter.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IApplicationDbContext, ResumeFilterDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
