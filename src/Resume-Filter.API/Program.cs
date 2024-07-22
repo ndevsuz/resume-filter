97857bae4826eb9001afd7190520b2b5dcd82d1a
Jurayevkh <jurayevkh@gmail.com>
2024-07-22 13:30:11 +0500

    Infrastructure layer add

--- src/Resume-Filter.API/Program.cs
+++ src/Resume-Filter.API/Program.cs
@@ -1,4 +1,8 @@
+using Resume_Filter.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
