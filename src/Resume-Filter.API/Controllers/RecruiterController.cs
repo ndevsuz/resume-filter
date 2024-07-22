a6cb466512f3fc5b02a64075e8ca73a7f49506ab
Jurayevkh <jurayevkh@gmail.com>
2024-07-22 21:00:46 +0500

    Recruiter Controller

--- src/Resume-Filter.API/Controllers/RecruiterController.cs
+++ src/Resume-Filter.API/Controllers/RecruiterController.cs
@@ -1,7 +1,12 @@
-﻿namespace Resume_Filter.API.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume_Filter.API.Helpers;
 
+namespace Resume_Filter.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
 public class RecruiterController
 {
-
+    
 }
 
