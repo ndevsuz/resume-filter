0732c7a78a4f0e45f5dd0793ee6975c2b6754c75
Jurayevkh <jurayevkh@gmail.com>
2024-07-23 03:25:52 +0500

    Vacancy Controller Done

--- src/Resume-Filter.API/Controllers/VacancyController.cs
+++ src/Resume-Filter.API/Controllers/VacancyController.cs
@@ -1,7 +1,69 @@
-﻿namespace Resume_Filter.API.Controllers;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Resume_Filter.API.Helpers;
+using Resume_Filter.Application.DTO.Vacancy;
+using Resume_Filter.Application.UseCases.Commands.Vacancy;
+using Resume_Filter.Application.UseCases.Queries.Vacancy;
 
-public class VacancyController
+namespace Resume_Filter.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class VacancyController:ControllerBase
 {
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
 
+    public VacancyController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAllVacancies()
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _mediator.Send(new GetAllVacancy())
+        });
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetVacancyById(int Id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _mediator.Send(new GetVacancyById() { Id = Id })
+        });
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateVacancy([FromForm]CreateVacancyDTO dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _mediator.Send(_mapper.Map<CreateVacancy>(dto))
+        });
+
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateVacancy([FromForm]UpdateVacancyDTO dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _mediator.Send(_mapper.Map<UpdateVacancy>(dto))
+        });
+
+    [HttpDelete]
+    public async ValueTask<IActionResult> DeleteVacancy(int Id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _mediator.Send(new DeleteVacancy() { Id = Id })
+        });
 }
 
