6cadd6a83cbf55dbbf0d18d29483dfab61a8c9ae
Jurayevkh <jurayevkh@gmail.com>
2024-07-23 03:29:26 +0500

    Id Prop added

--- src/Resume-Filter.Application/UseCases/Commands/Property/UpdateProperty.cs
+++ src/Resume-Filter.Application/UseCases/Commands/Property/UpdateProperty.cs
@@ -4,6 +4,7 @@ namespace Resume_Filter.Application.UseCases.Commands.Property;
 
 public class UpdateProperty:IRequest<bool>
 {
+    public int Id { get; set; }
     public int? VacancyId { get; set; }
     public string? Name { get; set; }
     public int? Value { get; set; }
