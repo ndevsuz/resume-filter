24c12932bc874fce8c1f9c1a718760e55dca7517
Iskandar Kodirov <eiscocyber@gmail.com>
2024-07-23 10:44:56 +0500

    gpt helper added

---
+++ src/Resume-Filter.Application/Helpers/GPTHelper.cs
@@ -0,0 +1,36 @@
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Resume_Filter.Application.Helpers;
+
+public static class GPTHelper
+{
+    
+    private static readonly string apiUrl = "https://api.openai.com/v1/chat/completions";
+    private static string apiKey = "sk-proj-66iLGZpmmlLT7RAjK5bET3BlbkFJN0k6oA941PtgHilLf5QM";
+    
+    public async static Task<string> SendRequest(string request)
+    {
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+            var requestBody = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+                    new { role = "user", content = request }
+                }
+            };
+
+            var content = new StringContent(JObject.FromObject(requestBody).ToString(), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(apiUrl, content);
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+        
+    }
+}
